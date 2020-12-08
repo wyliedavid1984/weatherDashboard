a27ba884c10e8dbe1aeddc4e0dbbb6b9770f3b7c
wylie <wyliedavid1984@gmail.com>
2020-12-08 10:01:24 -0500

    modified js to push data to array, from both local storage and user input. Also add conditional to check whether or not user input if null or already entered into list of cities.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    var weather = {
+
+    // http: //api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=34af04e7087783be92496c2a33100782
+    var cityWeather = {
         "city_name": "Custom location",
         "lat": 51.485927,
         "lon": 0.24995,
@@ -31,29 +33,84 @@ $(document).ready(function () {
         "dt_iso": "2020-03-31 00:00:00 +0000 UTC",
         "timezone": 3600
     }
+    var previousCities = JSON.parse(localStorage.getItem("userCities"))
 
+    console.log("getting cities " + previousCities);
+    if (previousCities === null) {
+        previousCities = [];
+        console.log("if === null " + previousCities);
+    } else {
+        for (var i = 0; i < previousCities.length; i++) {
+            $("#previousCity").prepend("<button>" + previousCities[i] + "</button><br>");
+        }
+    }
 
-    $(".today").text(weather.city_name+ " " +weather.dt_iso)
-    $(".jumbotron").html("<p></p>");
-    console.log(weather.city_name);
-    console.log(weather.dt_iso);
-
+    // getting information from weather api
+    $("#today").text(cityWeather.city_name + " " + cityWeather.dt_iso + cityWeather.weather.id);
+    $("#temperature").text("Temperature: " + cityWeather.main.feels_like);
+    $("#humidity").text("Humidity: " + cityWeather.main.humidity);
+    $("#windSpeed").text("Wind Speed: " + cityWeather.wind.speed);
+    $("#uvIndex").text("UV Index: " + cityWeather.main.temp)
 
 
     $(".btn").on("click", function () {
+
         // this actually grab what the user types in 
         var userCity = $(this).siblings("#searchBar").val();
-        console.log(userCity);
-        // this prepends user city to the empty li
-        $("#previousCity").prepend("<button>" + userCity + "</button><br>");
-        // this sets the user date into local storage with a key of time.
-        if (userCity !== localStorage.getItem(userCity)) {
-            localStorage.setItem(userCity, userCity);
+
+        // this check to see if user entered anything or if that city has already been enter, else prepends data to ul.
+        if (userCity === "") {
+            $("previousCities").prepend().html("<li> No City Enter Please Enter a City </li>").attr("id", "no")
+
+            setTimeout(() => {
+                    $("#no").css("display", "none");
+            }, 4000);
+
+
+            alert("No city entered");
+            return;
+        } else if (previousCities.includes(userCity)) {
+            alert("Please just click on the button")
+            console.log("else if " + userCity);
+        } else {
+            previousCities.push(userCity);
+            $("#previousCity").prepend("<button>" + userCity + "</button><br>").attr("city");
+            console.log("else prepend" + previousCities);
         }
+        console.log("fourth " + previousCities)
+        // this sets the user date into local storage with a key of time.
+        localStorage.setItem("userCities", JSON.stringify(previousCities))
+        console.log(JSON.stringify(previousCities));
+        $('#searchBar').val("");
+        
     });
 
+    function weatherBalloon(cityID) {
+        var key = '34af04e7087783be92496c2a33100782';
+        fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)
+            .then(function (resp) {
+                return resp.json()
+            }) // Convert data to json
+            .then(function (data) {
+                drawWeather(data);
+            })
+            .catch(function () {
+                // catch any errors
+            });
+    }
+   window.onload = function () {
+        weatherBalloon(previousCity[previousCity.length-1]);
+    }
 
 
+    function drawWeather(d) {
+        var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+        var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
 
-
+        $('#description').text(d.weather[0].description);
+        $('#temp').text(celcius + " c, " + fahrenheit + " f");
+        $('#location').text(d.name);
+    }
+console.log(previousCity[previousCity.length-1]);
 })
+    
