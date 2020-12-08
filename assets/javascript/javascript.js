9c9b7b5412fa48f8ff12542bcb737c759b448bac
wylie <wyliedavid1984@gmail.com>
2020-12-08 17:16:14 -0500

    have first data from api displaying. need to get second api to display.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,6 @@
 $(document).ready(function () {
 
-    var previousCities = JSON.parse(localStorage.getItem("userCities"))
-    var cityName;
-
-
-
+    var previousCities = JSON.parse(localStorage.getItem("userCities"));
     console.log("getting cities " + previousCities);
 
     if (previousCities === null) {
@@ -12,10 +8,13 @@ $(document).ready(function () {
         console.log("if === null " + previousCities);
     } else {
         for (var i = 0; i < previousCities.length; i++) {
-            $("#previousCity").prepend("<button>" + previousCities[i] + "</button><br>");
+            $("#previousCity").prepend("<br><button>" + previousCities[i]);
         }
+
     }
 
+    var cityName = previousCities[previousCities.length - 1];
+    weatherBalloon(cityName);
     // getting information from weather api
 
     $(".clear").on("click", function (event) {
@@ -23,7 +22,8 @@ $(document).ready(function () {
         localStorage.clear();
     })
 
-    $(".search").on("click", function () {
+    $(".search").on("click", function (event) {
+        event.preventDefault();
 
         // this actually grab what the user types in 
         var userCity = $(this).siblings("#searchBar").val();
@@ -42,53 +42,68 @@ $(document).ready(function () {
 
         } else {
             previousCities.push(userCity);
-            $("#previousCity").prepend("<button>" + userCity + "</button><br>").attr("city");
+            $("#previousCity").prepend("<br><button>" + userCity);
 
         }
         console.log("fourth " + previousCities)
         // this sets the user date into local storage with a key of time.
         localStorage.setItem("userCities", JSON.stringify(previousCities))
         console.log(JSON.stringify(previousCities));
-        $('#searchBar').val("");;
+        $('#searchBar').val("");
+        console.log(cityName);
+        weatherBalloon(cityName);
     });
-   
+
+    // $(".city").click(function (e) { 
+    //     e.preventDefault();
+    //     console.log(this.id);
+    //     $(this)
+
+    // });
+
 
     function weatherBalloon(city) {
-        var stringCity = JSON.stringify(city);
+
         var key = '34af04e7087783be92496c2a33100782';
-        var latLonURL = "http://api.openweathermap.org/data/2.5/weather?q="+stringCity+"&appid="+key
+        var latLonURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + key
         var lat;
         var lon;
-        var queryURL = "https: //api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=&appid="+key;
+        var queryURL= "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=&appid=" + key;
         $.ajax({
             url: latLonURL,
             method: "GET"
         }).then(function (response) {
             console.log(response);
-            console.log(response.coord.lon);
-            lon = response.coord.lon;
-            console.log(response.coord.lat);
-            lat = response.coord.lat;
-        })
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            console.log(response)
-            var celcius = Math.round(parseFloat(response.main.temp) - 273.15);
-            var fahrenheit = Math.round(((parseFloat(response.main.temp) - 273.15) * 1.8) + 32);
-            $("#today").text(response.city_name + " " + response.dt_iso + response.weather.id);
-            $("#temperature").text("Temperature: " + celcius + " c, " + fahrenheit + " f");
-            $("#humidity").text("Humidity: " + response.main.humidity);
-            $("#windSpeed").text("Wind Speed: " + response.wind.speed);
-            $("#uvIndex").text("UV Index: " + response.main.temp)
 
+            lon = JSON.stringify(response.coord.lon);
+            console.log(typeof (lon));
+            lat = JSON.stringify(response.coord.lat);
+            console.log(typeof (lat));
+            
         })
- 
+        console.log(lat);
+        console.log(lon);
+        if (lat !== undefined && lon !== undefined) {
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                console.log(response)
+                var celcius = Math.round(parseFloat(response.main.temp) - 273.15);
+                var fahrenheit = Math.round(((parseFloat(response.main.temp) - 273.15) * 1.8) + 32);
+                $("#today").text(response.city_name + " " + response.dt_iso + response.weather.id);
+                $("#temperature").text("Temperature: " + celcius + " c, " + fahrenheit + " f");
+                $("#humidity").text("Humidity: " + response.main.humidity);
+                $("#windSpeed").text("Wind Speed: " + response.wind.speed);
+                $("#uvIndex").text("UV Index: " + response.main.temp)
+
+            })
+        }
+
     }
-   weatherBallon(cityName)
 
- // window.onload = function () {
+
+    // window.onload = function () {
     //     weatherBalloon(previousCity[previousCity.length - 1]);
     // }
 
